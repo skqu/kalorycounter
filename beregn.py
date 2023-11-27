182a92aaa731625c3c457cf4c4c788e379109e24
Nick6480 <nickolaj99@outlook.com>
2023-11-27 12:11:06 +0100

    added exeption handling

--- beregn.py
+++ beregn.py
@@ -8,17 +8,21 @@ class beregn():
     #Calculate kcal
     def calc_kcal(self, gender : str, age : int, height : float, weight : float):
         gender = gender.lower()
+        bmr = 0
 
         try:
             if gender in self.genders['male']: 
-                bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
-            
+                    bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+                
             elif gender in self.genders['female']:
-                bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+                    bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
 
+            else: 
+                print('calc_kcal: Unknown gender specified')
+            
             return bmr
         except Exception as e:
-            print('Failed to calculate kcal: ', e)
+            print('Failed to calculate Kcal:', e)
             
 
 
@@ -26,4 +30,4 @@ class beregn():
 
 if __name__ == '__main__':
     calc = beregn()
-    print(calc.calc_kcal('mn', 24, 174, 70))
+    print(calc.calc_kcal('man', 24, 174, 70))
