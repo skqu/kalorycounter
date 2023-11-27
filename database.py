1fbc77bda1571f4fcd7faafad6b9f46b8a1dfe3c
Fahadawad <fahad.awad1505@icloud.com>
2023-11-27 13:39:49 +0100

    add

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import mysql.connector
+from datetime import date
 
-host = "localhost"
+host = "127.0.0.1"
 port = 3306
 user = "root"
 password = ""
-database = "kalorieberegner"
+database = "KB"
 
 conn = mysql.connector.connect(
     host=host,
@@ -17,20 +18,21 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 create_table_query = """
+DROP TABLE IF EXISTS kalorieberegner;
 CREATE TABLE IF NOT EXISTS kalorieberegner (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     birthday DATE,
     gender ENUM('M', 'F', 'Other'),
     height FLOAT,
-    weight FLOAT
+    weight FLOAT,
+    Amr FLOAT
 )
 """
 
-cursor.execute(create_table_query)
+cursor.execute(create_table_query, multi=True)
 conn.commit()
 
-# Check if the table exists by describing its structure
 check_table_query = "DESCRIBE kalorieberegner"
 try:
     cursor.execute(check_table_query)
@@ -40,6 +42,29 @@ try:
         print("Table 'kalorieberegner' exists.")
     else:
         print("Table 'kalorieberegner' does not exist.")
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+try:
+    # Person 1
+    insert_query1 = """
+    INSERT INTO kalorieberegner (name, birthday, gender, height, weight, Amr)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    data1 = ("Fahad", date(1990, 1, 1), 'M', 175.0, 70.0, 0.0)
+    cursor.execute(insert_query1, data1)
+
+    # Person 2
+    insert_query2 = """
+    INSERT INTO kalorieberegner (name, birthday, gender, height, weight, Amr)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    data2 = ("Awad", date(1985, 5, 15), 'F', 160.0, 55.0, 0.0)
+    cursor.execute(insert_query2, data2)
+
+    conn.commit()
+    print("To personer tilføjet til databasen.")
 
 except mysql.connector.Error as err:
     print(f"Error: {err}")
